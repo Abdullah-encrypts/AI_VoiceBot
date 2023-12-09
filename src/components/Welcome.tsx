9fb08cf4cdcc17b03c9f41a00828fd8592c1949b
Abdullah-encrypts <Dev.AbdullahAwan@gmail.com>
2023-12-09 17:11:09 +0500

    first commit

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+export const Welcome: React.FC = () => (
+  <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
+    <h1 className="text-2xl font-bold mb-2">Welcome to the voice based chat bot!</h1>
+    <p>
+      I am a voice bot! You can press the mic button below to send me voice messages and i will respond with your querries!
+    </p>
+    <p>
+    </p>
+  </div>
+);
